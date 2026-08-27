91ec79bea4bba4d43c22cf9740366c8d203b3d14
tenderoalbertalberto-rgb <tenderoalbertalberto@gmail.com>
2026-08-27 09:17:30 +0200

    Implement service worker cache management
    Added activate event listener to manage cache updates.

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
 const CACHE_NAME = 'pricecalc-pro-v1';
 const urlsToCache = [
   './',
-  './pricecalc.html',
-  './manifest.json',
-  './licenses.json'
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
